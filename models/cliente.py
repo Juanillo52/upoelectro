7ab6fb55ea764b324bc1c279c3754773d046d386
AndresManuelCH <7bamcm@gmail.com>
2019-11-29 14:32:03 +0100

    Añadido a cliente, compra, empleado, proveedor y venta los campos codigo postal, ciudad y provincia, actualizadas sus respectivas vistas para que aparezcan los campos en la vista form y no en tree. Actualizadas también las vistas para que las fotos salgan solo en las vistas form

--- models/cliente.py
+++ models/cliente.py
@@ -8,5 +8,8 @@ class cliente(models.Model):
     telefono = fields.Char('Teléfono', size=9)
     correo = fields.Char('Correo', size=40)
     direccion = fields.Char('Dirección', size=60)
+    codigo_postal = fields.Char('Código postal', size=5)
+    ciudad = fields.Char('Ciudad', size=60)
+    provincia = fields.Char('Provincia', size=60)
     foto = fields.Binary('Foto de perfil')
     ventas_ids = fields.One2many('upoelectro.venta','cliente_id', 'Ventas') 
